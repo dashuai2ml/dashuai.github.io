73b98c8ddf4b273b1df1a73e991b627dc143cb6c
zhangxu <jshpy@163.com>
2023-04-20 18:27:03 +0800

    update

--- src/components/__tests__/MDArticleTemplate.test.ts
+++ src/components/__tests__/MDArticleTemplate.test.ts
@@ -4,7 +4,8 @@ import { test, expect } from 'vitest'
 
 test('mount markdown component', async () => {
   expect(MD).toBeTruthy()
-  const wrapper = mount(MD, { props: { content: 'test\n=' } })
+  const wrapper = mount(MD, { props: { filePath: '/src/assets/article/test.md' } })
+  console.log(wrapper.html());
   expect(wrapper.html()).toContain('h1')
 })
 
