dc9e152b1d55a32e2b29f36061ecf4cb767d8c9a
zhangxu <jshpy@163.com>
2023-05-17 18:23:39 +0800

    update

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -2,7 +2,7 @@
   <div id="content">
     <div class="text">
       <span>
-        <img id="hi" src="@/assets/png/Waving hand.png" alt="" srcset="">
+        <img id="hi" src="@/assets/png/WavingHand.png" alt="" srcset="">
       </span>
       <p>
         我是一名热爱技术、热爱生活的开发者和程序员。对于新鲜事物有着强烈的探索欲望和求知欲，同时也非常热衷于不断学习和提高自己。在工作和生活中，我总是尽力将自己的思考和创造力发挥到极致，以应对日常的挑战并创造更多的价值。
@@ -10,7 +10,7 @@
     </div>
     <div class="text">
       <span>
-        <img src="@/assets/png/Zany face.png" alt="" srcset="">
+        <img src="@/assets/png/ZanyFace.png" alt="" srcset="">
       </span>
       <p>
         I'm a developer and programmer who loves technology and life. I have a strong desire to explore and learn
