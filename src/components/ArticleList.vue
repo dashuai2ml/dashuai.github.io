35f973cbc839d59decb4c22aad56a727ec027be3
dashuai2ml <jshpy@163.com>
2023-06-03 18:56:25 +0800

    update

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -2,16 +2,16 @@
 <template>
   <div class="flex flex-col items-center">
     <div class="flex flex-col rounded-md mb-3 justify-center sm:p-10 p-5 sm:w-[700px] w-[300px] sm:h-[100px] bg-white hover:drop-shadow-md transition-all duration-300">
-      <a href="https://dashuai2ml.notion.site/ChatGPT-Text2Img-AI-ef0f1a8f39c946008a1c98ecb3386995" target="_blank"><p class=" font-bold">以ChatGPT和Text2Img为代表的AI大模型是否能够引起一次生产力革命？</p></a>
-      <p>通过分析它们在自然语言生成和图像生成方面的创新功能，文章讨论了它们如何为自动化任务、内容生成和创造性领域带来巨大潜力。</p>
+      <a href="https://dashuai2ml.notion.site/ChatGPT-Text2Img-AI-ef0f1a8f39c946008a1c98ecb3386995" target="_blank"><p class=" font-bold">以 ChatGPT 和 Text2Img 为代表的 AI 大模型能否引起一次生产力革命？</p></a>
+      <p class="text-gray-500">通过分析它们在自然语言生成和图像生成方面的创新功能，文章讨论了它们如何为自动化任务、内容生成和创造性领域带来巨大潜力。</p>
     </div>
     <div class="flex flex-col rounded-md mb-3 justify-center sm:p-10 p-5 sm:w-[700px] w-[300px] sm:h-[100px] bg-white hover:drop-shadow-md transition-all duration-300">
       <a href="https://dashuai2ml.notion.site/Tailwind-CSS-bbf1bbc01ec24b0fb654087756bccbd1" target="_blank"><p class=" font-bold">对 Tailwind CSS 的改观</p></a>
-      <p>Tailwind CSS 通过基于类的样式定义和实用程序性样式的组合，大大简化了样式管理的过程，在开发速度、可维护性和可扩展性方面成为现代前端开发的新趋势。</p>
+      <p class="text-gray-500">Tailwind CSS 通过基于类的样式定义和实用程序性样式的组合，大大简化了样式管理的过程，在开发速度、可维护性和可扩展性方面成为现代前端开发的新趋势。</p>
     </div>
     <div class="flex flex-col rounded-md mb-3 justify-center sm:p-10 p-5 sm:w-[700px] w-[300px] sm:h-[100px] bg-white hover:drop-shadow-md transition-all duration-300">
       <a href="https://dashuai2ml.notion.site/71f9448765614e61b894081d656f3317" target="_blank"><p class=" font-bold">对人类文明进程的思考</p></a>
-      <p>通过分析人类社会的技术进步、科学发现和文化变革，文章呈现了人类文明进程中的关键转折点和里程碑。</p>
+      <p class="text-gray-500">通过分析人类社会的技术进步、科学发现和文化变革，文章呈现了人类文明进程中的关键转折点和里程碑。</p>
     </div>
     <p><a href="https://dashuai2ml.notion.site/d8b4afed09674129ab970efa00fe084a" target="_blank">查看更多>></a></p>
   </div>
